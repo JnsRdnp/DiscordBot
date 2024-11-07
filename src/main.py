3cf4b204cf13297b14c468c776868d547ac75b1d
unknown <joonasroska123@gmail.com>
2024-11-07 16:14:56 +0200

    on_message interfered with commands

--- src/main.py
+++ src/main.py
@@ -8,13 +8,20 @@ load_dotenv()
 bot_token = os.getenv("BOT_TOKEN")
 
 
-bot = commands.Bot(command_prefix='.', intents=discord.Intents.all())
+intents = discord.Intents().all()
+intents.message_content= True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 
 @bot.event
 async def on_ready():
     print("Bot is ready.")
 
+@bot.command()
+async def hello(ctx):
+    print('In "hello" function')
+    await ctx.send("Hello, I am PompivaBot.")
 
 
 bot.run(bot_token)
